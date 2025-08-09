c59c2054ff1ec345c0ff507f7a3259e4f0ad37ab
thisisrow <prathameshmisra2020@gmail.com>
2025-08-09 06:46:32 +0530

    middlware

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config');
+const User = require('../models/User');
+
+async function auth(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ message: 'Missing Authorization' });
+
+  const token = authHeader.replace('Bearer ', '');
+  
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(payload.id);
+  
+    if (!user) return res.status(401).json({ message: 'Invalid token' });
+    req.user = user;
+  
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
+
+module.exports = auth;
